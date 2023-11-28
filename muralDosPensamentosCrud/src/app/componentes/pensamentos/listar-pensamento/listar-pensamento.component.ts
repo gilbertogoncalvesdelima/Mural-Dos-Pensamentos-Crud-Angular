60264897db5562f312d88993f9d7e36f1f7cbd8e
gilberto goncalves de lima <gilberto-goncalves@outlook.com.br>
2023-11-28 14:25:38 -0300

    Comunicacao entre componentes, decorator, @input

--- muralDosPensamentosCrud/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ muralDosPensamentosCrud/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -5,11 +5,22 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './listar-pensamento.component.html',
   styleUrls: ['./listar-pensamento.component.css']
 })
+//Componente Pai
 export class ListarPensamentoComponent implements OnInit {
 
  //Criado um atributo chamado listaPensamentos
   listaPensamentos = [
-
+    // Teste Lista
+  {
+    conteudo: "Passo informação para o componente filho",
+    autoria: "Componente pai",
+    modelo: "modelo3"
+  },
+  {
+    conteudo: "Minha propriedade é decorada com @Input()",
+    autoria: "Componente Filho",
+    modelo: "modelo2"
+  }
   ]
 
   constructor() { }
--- muralDosPensamentosCrud/src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ muralDosPensamentosCrud/src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,18 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pensamento',
   templateUrl: './pensamento.component.html',
   styleUrls: ['./pensamento.component.css']
 })
+// Componente Filho
 export class PensamentoComponent implements OnInit {
 
   // Criamos um atributos, chamado pensamento, que sera um objeto com um conteudo
-  pensamento = {
+  // Foi utilizado o decorator, @Input, no html foi feito um property binding, da propriedade pensamento, no html do listar pensamentos e ira receber informações do componente Pai
+  @Input() pensamento = {
   conteudo: "I love Angular",
   autoria: 'Gilberto',
   modelo: 'modelo3'
-
   }
 
   constructor() { }
