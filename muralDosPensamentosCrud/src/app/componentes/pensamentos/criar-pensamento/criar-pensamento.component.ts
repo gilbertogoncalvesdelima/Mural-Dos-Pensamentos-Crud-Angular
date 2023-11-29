21820c8eec3c6c9a92bf1c5e87e29ced09367b55
gilberto goncalves de lima <gilberto-goncalves@outlook.com.br>
2023-11-29 17:46:28 -0300

    formBuilder, class de serviço e chamando apos a inicialização do ngOnInit

--- muralDosPensamentosCrud/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ muralDosPensamentosCrud/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,6 +2,7 @@ import { PensamentoService } from './../pensamento.service';
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -17,13 +18,22 @@ export class CriarPensamentoComponent implements OnInit {
     autoria: '',
     modelo: 'modelo1'
   }
+  // FormGrupo, ele serve para construção de formulários, que o angular nos fornece
+  formulario!: FormGroup;
 
   constructor(private service: PensamentoService,
-  private router: Router
+  private router: Router,
+  // FormBuilder, ele é outra class de serviço, responsável pela construção do formulário
+  private formBuilder: FormBuilder
     ) { }
 
   // NgOnInit, Para que serve: Ele é chamado uma única vez após a inicialização do componente.
   ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      conteudo:['Formulario reativo'],
+      autoria: [''],
+      modelo: ['modelo1']
+    })
   }
   criarPensamento() {
   this.service.criar(this.pensamento).subscribe(() => {
